2264856298c79a071b829b00ebb8c18bc600cd04
Joel Christensen <joelcnz@gmail.com>
2011-05-06 12:43:05 +1200

    Changed compile files batch file (cf.bat) a bit

--- cf.bat
+++ cf.bat
@@ -1,5 +1,5 @@
 @echo off
-rem src = abc5 .. doesn't work
+rem src = abc5 .. doesn't work, has to be src=abc5 (no spaces)
 set src=abc5 base world media input keyhandling texthandling current
 echo cf -r  for release version
 echo cf -d  for creating html files
