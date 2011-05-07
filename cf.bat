cef2447e65a335b4c33ef7a66d27b982a38e4298
Joel Christensen <joelcnz@gmail.com>
2011-05-08 02:11:52 +1200

    Added a .sh file, and got rid of a artifact

--- cf.bat
+++ cf.bat
@@ -4,16 +4,19 @@ set src=abc5 base world media input keyhandling texthandling current
 echo cf -r  for release version
 echo cf -d  for creating html files
 echo cf     default developing
+
 if "%1"=="" (
 echo ----- Doing a version for WIP ------
-dmd %src% abc.res -debug -debug=TDD -I\jpro\dpro2\import libjeca.lib
+dmd %src% abc.res -debug -debug=TDD libjeca.lib
 )
+
 if "%1"=="-r" (
 echo ------ Doing a version that runs without a terminal ------
 dmd %src% abc.res looseterm.def
 del *.obj
 del *.map
 )
+
 if "%1"=="-d" (
 echo ------ Doing documentation ------
 echo open html folder, select all files, then right click open. Note: Firefox doesn't know what to do
---
+++ cf.sh
@@ -0,0 +1,1 @@
+dmd abc5 base world media input keyhandling texthandling current abc.res -debug -debug=TDD libjeca.lib
